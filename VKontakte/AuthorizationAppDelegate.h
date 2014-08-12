6544fc52f478e1b8e023fe581eb8b824cef6d664
Oleg Pochtovy <opochtovy@yahoo.com>
2014-08-12 10:26:00 +0300

    Initial commit

---
+++ VKontakte/AuthorizationAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AuthorizationAppDelegate.h
+//  VKontakte
+//
+//  Created by Oleg Pochtovy on 18.07.14.
+//  Copyright (c) 2014 Oleg Pochtovy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AuthorizationAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
