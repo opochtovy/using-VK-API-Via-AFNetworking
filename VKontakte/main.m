fc78aa00546427462818facfaee5bccad089c524
Oleg Pochtovy <opochtovy@yahoo.com>
2014-08-12 10:29:35 +0300

    API illustrates the work with a web-service API (VK.com API) via AFNetworking

---
+++ VKontakte/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  VKontakte
+//
+//  Created by Oleg Pochtovy on 18.07.14.
+//  Copyright (c) 2014 Oleg Pochtovy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AuthorizationAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AuthorizationAppDelegate class]));
+    }
+}
