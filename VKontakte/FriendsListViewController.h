333551d51885ab635fdb9c9e5dbbcd5cfb5c579d
Oleg Pochtovy <opochtovy@yahoo.com>
2014-08-12 10:45:11 +0300

    Class to view list of friends in social network VKontakte

---
+++ VKontakte/FriendsListViewController.h
@@ -0,0 +1,17 @@
+//
+//  FriendsListViewController.h
+//  VKontakte
+//
+//  Created by Oleg Pochtovy on 07.08.14.
+//  Copyright (c) 2014 Oleg Pochtovy. All rights reserved.
+//
+
+// Class to view list of friends in social network VKontakte
+
+#import <UIKit/UIKit.h>
+
+@interface FriendsListViewController : UITableViewController
+
+- (void)getFriends;
+
+@end
